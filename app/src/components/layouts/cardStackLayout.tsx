2dde35bbbd9b43637b0a8b553b2df92f3fe0f52e
daachan <daichan2416@gmail.com>
2026-08-02 21:14:45 +0900

    refactor: 新レイアウトに対応した交通費申請コンポーネント

--- app/src/components/layouts/cardStackLayout.tsx
+++ app/src/components/layouts/cardStackLayout.tsx
@@ -5,58 +5,67 @@ import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 
-function CardStackLayout({ children, addCardHandler }: { children: React.ReactNode[], addCardHandler: () => void }) {
+interface CardStackLayoutProps {
+  children: React.ReactNode;
+  addCardHandler: () => void;
+}
+
+function CardStackLayout({
+  children,
+  addCardHandler,
+}: CardStackLayoutProps) {
   return (
-    <Container 
-      maxWidth="md" 
-      sx={{ 
-        height: '100%', 
-        overflow: 'hidden', 
-        display: 'flex', 
-        justifyContent: 'center' 
+    <Container
+      maxWidth="md"
+      sx={{
+        height: '100%',
+        overflow: 'hidden',
+        display: 'flex',
+        justifyContent: 'center',
       }}
     >
       <Stack
-        spacing={3} 
+        spacing={3}
         sx={{
-          width: '100%', 
-          alignItems: 'center', 
-          overflowY: 'auto',  
-          overflowX: 'hidden', 
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          overflowY: 'auto',
+          overflowX: 'hidden',
           py: 4,
-          px: 1,               
-          boxSizing: 'border-box' 
+          px: 1,
+          boxSizing: 'border-box',
         }}
       >
-        {children.map((child, index) => (
+        {React.Children.map(children, (child, index) => (
           <Box
             key={index}
             sx={{
               width: '100%',
               maxWidth: '100%',
               flexShrink: 0,
-              aspectRatio: '2.1 / 1', 
+              aspectRatio: '2.1 / 1',
               boxSizing: 'border-box',
-              display: 'flex', 
+              display: 'flex',
               '& > *': {
                 borderRadius: '8px',
-                width: '100%',   
-                height: '100%', 
-                objectFit: 'cover' 
-              }
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+              },
             }}
           >
             {child}
           </Box>
         ))}
 
-        <Button 
-          variant="outlined" 
+        <Button
+          variant="outlined"
           fullWidth
           onClick={addCardHandler}
-          sx={{ 
-            minHeight: '30px', 
-            flexShrink: 0,   
+          sx={{
+            minHeight: 48,
+            flexShrink: 0,
             borderRadius: '8px',
             borderStyle: 'dashed',
             borderWidth: '2px',
@@ -66,7 +75,7 @@ function CardStackLayout({ children, addCardHandler }: { children: React.ReactNo
         </Button>
       </Stack>
     </Container>
-  )
+  );
 }
 
-export default CardStackLayout
+export default CardStackLayout;
